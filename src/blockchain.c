6af69cc40935201a0d9ca1285d2219440120accb
Karan Nedungadi <karannedungadi@my.capilanou.ca>
2018-03-16 15:23:43 +0000

    Completed v0.1 of bc ADT

--- src/blockchain.c
+++ src/blockchain.c
@@ -2,7 +2,7 @@
  *  BlockChain ADT : private implementation / algorithms
  *
  *  COMP220: Assignment 3
- *  Author:  
+ *  Author:  Karan Nedunagdi
  *  Date:    Feb. 1, 2018
  */
 
@@ -22,3 +22,104 @@
  * BLOCKCHAIN private implementation details
  ***********************/
 const int DEFAULT_DIFFICULTY = 2;              // Default difficulty for hashing puzzles, increase to make mining harder
+
+void bcPop(BlockChain* chain);
+bool bcIsEmpty(BlockChain chain);
+void blkPrint(Block_t blk);
+
+/*
+ * prints a block on STDOUT
+ */
+void blkPrint(Block_t blk) {
+    printf("----------------- \n");
+    printf("|\tBlock: # %d\t|\n", blk.id);
+    printf("|\tNonce: %d  \t|\n", blk.proof_of_work.nonce);
+    tlistPrint(blk.transactions);
+    printf("|\tPrev:  %s  \t|\n", blkPrevHash(blk));
+    printf("|\tPrev:  %s  \t|\n", blk.hash);
+    printf("----------------- \n");
+    printf("-->");
+}
+
+BlockChain bcNew() {
+    Block_t* new = blkCreate(tlistCreate(), DEFAULT_DIFFICULTY, NULL_NONCE);
+    BlockChain bc = {new, new};
+}
+
+void bcDelete(BlockChain *chain) {
+    assert(bcLen(*chain) > 0);
+    while(chain->head->next != NULL) {
+        bcPop(chain);
+    }
+}
+
+/*
+ * Delete the block at the front of the list
+ * PRE: make sure list is not empty
+ */
+void bcPop(BlockChain* chain) {
+    assert(!bcIsEmpty(*chain));
+    Block_t* blk = chain->head->next;
+    chain->head->next = blk->next;
+    if(chain->tail == blk) {
+        chain->tail = chain->head;
+    }
+    blkDelete(blk);
+}
+
+void bcPrint(const BlockChain chain) {
+    Block_t*cur = chain.head->next;
+    printf("\n BlockChain:\n");
+    while(cur != NULL) {
+        blkPrint(*cur);
+        printf("\n");
+        cur = cur->next;
+    }
+    printf("\n\n");
+}
+
+int bcLen(const BlockChain chain) {
+    int len = 0;
+    Block_t* cur = chain.head->next;
+    while(cur != NULL) {
+        cur = cur->next;
+        len++;
+    }
+    return len;
+}
+
+bool bcIsValid(const BlockChain chain) {
+    Block_t* cur = chain.head->next;
+    while(cur != NULL) {
+        if(blkIsValid(*cur))
+            cur = cur->next;
+            else {
+                return false;
+            }
+    }
+    return true;
+}
+
+Block_t* bcTail(const BlockChain chain) {
+    return chain.tail;
+}
+
+/*
+ * Checks if the chain is empty
+ */
+bool bcIsEmpty(BlockChain chain) {
+    return chain.head->next == NULL;
+}
+
+void bcAppend(BlockChain *chain, Block_t* new_block) {
+    if(bcIsEmpty(*chain)) {
+        chain->head->next = chain->tail = new_block;
+    }
+    else {
+        /*new_block->next = chain->tail->next;
+        chain->tail->next = new_block;
+        chain->tail = new_block;*/
+        blkChainTo(chain->tail, new_block);
+    }
+    assert(bcTail(*chain) == new_block && blkIsValid(*new_block));
+}
